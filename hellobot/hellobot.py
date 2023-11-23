ad91bd348eee4e0e510b96fa52e6c600f6a82f6b
Goldiefish <hatchjenjen@gmail.com>
2023-11-22 21:50:49 -0600

    finally added the welcome feature

--- hellobot/hellobot.py
+++ hellobot/hellobot.py
@@ -6,6 +6,12 @@ class HelloBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send(f'Welcome {member.mention}!!')
+
     @commands.command()
     async def hello(self, ctx):
         """This does stuff!"""
