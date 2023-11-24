7a00e8c88f3350f54f5a6c366694e350ef535e76
James Van Volkenburgh <jvanvolken1@gmail.com>
2023-11-23 23:13:29 -0600

    Test position of circle

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -102,7 +102,8 @@ class Welcome(commands.Cog):
             resized_width, resized_height = resized_avatar.size
 
             # Draw circle around avatar image
-            draw.ellipse((0, 0) + resized_avatar.size, outline = (209, 202, 192, 255), width = 20)
+            position = (round((background_width - resized_width)/2), round(margins * 1.3))
+            draw.ellipse(position + resized_avatar.size, outline = (209, 202, 192, 255), width = 10)
 
             # Construct a circular mask for the avatar image
             mask = Image.new('L', resized_avatar.size, 0)
