0a3d5783009ac9509c517bf424f8142a10d7ecf0
James Van Volkenburgh <jvanvolken1@gmail.com>
2023-11-23 19:35:26 -0600

    Created a new cog for loading of both

---
+++ welcome/welcome.py
@@ -0,0 +1,21 @@
+from redbot.core import commands
+
+class Welcome(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # @commands.Cog.listener()
+    # async def on_member_join(self, member):
+    #     channel = member.guild.system_channel
+    #     userAvatarUrl = member.display_avatar
+    #     if channel is not None:
+
+    #         await channel.send(f'Welcome {member.mention}!!\n{userAvatarUrl}')
+
+    @commands.command()
+    async def test_cmd(self, ctx):
+        """This responds to the text command!"""
+        channel = ctx.channel
+        await channel.send("Hello {ctx.message.author.mention}!")
